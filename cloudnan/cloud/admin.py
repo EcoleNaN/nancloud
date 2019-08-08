4de550a1061df9d6d61498f2967d0fbd5344946a
Soro08 <soro@nan.ci>
2019-08-08 14:07:26 +0200

    Initial commit

---
+++ cloudnan/cloud/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+#Configuration du site django
+admin.site.site_header = "NaN CLoud" 
+admin.site.site_title = "NaN Cloud Admin Portal"
+admin.site.index_title = "Welcome to N-C Admin Portal"
